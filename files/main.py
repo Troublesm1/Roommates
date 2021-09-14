3e33f431eea1b30a844945a1eee116a37065deb7
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-09-14 11:46:46 -0400

    PdfReport class added

--- files/main.py
+++ files/main.py
@@ -14,3 +14,12 @@ class Roommate:
 
     def pays(self, bill):
         pass
+
+class PdfReport:
+    """ Creates a Pdf file that contains data about the flatmates such as their names, their amount and the billing period"""
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, roommate1, roommate2, bill):
+        pass
