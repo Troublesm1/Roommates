afb3305555c07f6deae29bfc1e64245998c55fbf
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-10-10 23:14:53 -0400

    Code refactor

---
+++ files/roommate.py
@@ -0,0 +1,16 @@
+class Roommate:
+    """ Creates a roommate(person) who lives in the flat and pays a share of the bill."""
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill, roommate2):
+        weight = self.days_in_house / (self.days_in_house + roommate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
+
+class Bill:
+    """ Object that contains data about a bill, such as the total amount and period of the bill."""
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
